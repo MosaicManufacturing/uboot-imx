c0cbc177028744f72cf829a0dcdfc4d6e370b2c8
Jonathan Liu <112430865+jonathan-liu-65536@users.noreply.github.com>
2023-03-01 14:33:38 -0500

    remove boot delay (#1)

--- include/configs/imx8mm_var_dart.h
+++ include/configs/imx8mm_var_dart.h
@@ -81,6 +81,7 @@
 	"m4_addr=0x7e0000\0" \
 	"m4_bin=hello_world.bin\0" \
 	"use_m4=no\0" \
+	"bootdelay=0\0" \
 	"loadm4bin=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${m4_bin}; " \
 		"cp.b ${loadaddr} ${m4_addr} ${filesize}\0" \
 	"runm4bin=" \
